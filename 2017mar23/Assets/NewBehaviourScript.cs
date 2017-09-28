62bd512110a0a9f50c49ea1cffa8b8b04ba63eaa
ethus3h <kolubat@gmail.com>
2017-09-28 14:26:12 -0400

    Update Ember tree

---
+++ 2017mar23/Assets/NewBehaviourScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    // Use this for initialization
+    void Start() {
+        Debug.Log("Yo");
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.mass = 10;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
