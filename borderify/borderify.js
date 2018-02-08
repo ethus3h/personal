92ca223a8b5a52b632085d6c505e56de56edbafc
ethus3h <kolubat@gmail.com>
2018-02-08 11:49:37 -0500

    Update Ember tree

--- borderify/borderify.js
+++ borderify/borderify.js
@@ -2,3 +2,31 @@ browser.webRequest.onBeforeRequest.addListener(
     blockImages,
     {urls: ["<all_urls>"], types:["image","imageset"]}, ["blocking"]
 );
+
+PageStore.prototype.filterLargeMediaElement = function(size) {
+    this.logData = undefined;
+
+    if ( Date.now() < this.allowLargeMediaElementsUntil ) {
+        return 0;
+    }
+    if ( µb.hnSwitches.evaluateZ('no-large-media', this.tabHostname) !== true ) {
+        return 0;
+    }
+    if ( (size >>> 10) < µb.userSettings.largeMediaSize ) {
+        return 0;
+    }
+
+    this.largeMediaCount += 1;
+    if ( this.largeMediaTimer === null ) {
+        this.largeMediaTimer = vAPI.setTimeout(
+            this.injectLargeMediaElementScriptlet.bind(this),
+            500
+        );
+    }
+
+    if ( µb.logger.isEnabled() ) {
+        this.logData = µb.hnSwitches.toLogData();
+    }
+
+    return 1;
+};
