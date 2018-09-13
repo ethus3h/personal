06abc5f6029f100d4c1239cb152f9490257853ee
ethus3h <kolubat@gmail.com>
2018-09-13 13:26:53 -0400

    Update Git repository using egup-single

--- work/Fall 2018/NMD498/2018sept13-Diagram.mm
+++ work/Fall 2018/NMD498/2018sept13-Diagram.mm
@@ -4,6 +4,154 @@
 <edge COLOR="#808080" STYLE="bezier"/>
 <font NAME="WreatheSystem" SIZE="20"/>
 <hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1536857827313" ID="ID_1663966613" MODIFIED="1536859203161" POSITION="left" STYLE="bubble" TEXT="Community">
+<edge COLOR="#808080" STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="WreatheSystem" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1536858230493" ID="ID_776110827" MODIFIED="1536859203161" STYLE="bubble" TEXT="Community development process">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="WreatheSystem" SIZE="16"/>
+<node COLOR="#990000" CREATED="1536858247347" ID="ID_1022438097" MODIFIED="1536859203161" STYLE="bubble" TEXT="Human support">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="WreatheSystem" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1536858262370" ID="ID_1027927813" MODIFIED="1536859203161" STYLE="bubble" TEXT="Money">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="WreatheSystem" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1536858271888" ID="ID_1204484055" MODIFIED="1536859203161" STYLE="bubble" TEXT="Stability">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="WreatheSystem" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1536858299231" ID="ID_840067514" MODIFIED="1536859203161" STYLE="bubble" TEXT="Infrastructure">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="WreatheSystem" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1536858237135" ID="ID_1543965600" MODIFIED="1536859203161" STYLE="bubble" TEXT="Land">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="WreatheSystem" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1536858327650" ID="ID_323325431" MODIFIED="1536859203161" STYLE="bubble" TEXT="Specification development work">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="WreatheSystem" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1536858338680" ID="ID_824587110" MODIFIED="1536859203161" STYLE="bubble" TEXT="Technology development work">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="WreatheSystem" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1536857814458" ID="ID_1475420914" MODIFIED="1536859203162" POSITION="left" STYLE="bubble" TEXT="Information technology environment">
+<edge COLOR="#808080" STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="WreatheSystem" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1536858051722" ID="ID_1387187635" MODIFIED="1536859203161" STYLE="bubble" TEXT="Client app">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="WreatheSystem" SIZE="16"/>
+<node COLOR="#990000" CREATED="1536859002631" ID="ID_692537468" MODIFIED="1536859203161" STYLE="bubble" TEXT="...">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="2"/>
+<font NAME="WreatheSystem" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1536858987509" ID="ID_1080877014" MODIFIED="1536859203161" STYLE="bubble" TEXT="Complex text rendering">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="WreatheSystem" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1536858998764" ID="ID_1291510183" MODIFIED="1536859203161" STYLE="bubble" TEXT="Data storage and distribution">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="WreatheSystem" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1536858993958" ID="ID_91462065" MODIFIED="1536859203161" STYLE="bubble" TEXT="Networking">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="WreatheSystem" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1536858982884" ID="ID_1689569" MODIFIED="1536859203161" STYLE="bubble" TEXT="Simple document editor">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="WreatheSystem" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1536858873075" ID="ID_1218950878" MODIFIED="1536859203161" STYLE="bubble" TEXT="Simple text rendering">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="WreatheSystem" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1536858966592" ID="ID_1121684754" MODIFIED="1536859203161" STYLE="bubble" TEXT="StageL in EITE">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="WreatheSystem" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1536858977744" ID="ID_378862452" MODIFIED="1536859203161" STYLE="bubble" TEXT="UI widgets">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="WreatheSystem" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1536858043185" ID="ID_879988637" MODIFIED="1536859203162" STYLE="bubble" TEXT="Data formats">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="WreatheSystem" SIZE="16"/>
+<node COLOR="#990000" CREATED="1536858144616" ID="ID_604776499" MODIFIED="1536859203162" STYLE="bubble" TEXT="Reference implementations">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="WreatheSystem" SIZE="14"/>
+<node COLOR="#111111" CREATED="1536858747930" ID="ID_1465504453" MODIFIED="1536859203162" STYLE="bubble" TEXT="EITE document format RI">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="WreatheSystem" SIZE="12"/>
+<node COLOR="#111111" CREATED="1536858755982" ID="ID_199768905" MODIFIED="1536859203162" STYLE="bubble" TEXT="Dc list form">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="WreatheSystem" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1536858845994" ID="ID_245871844" MODIFIED="1536859203162" STYLE="bubble" TEXT="Text source form">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="WreatheSystem" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1536858653579" ID="ID_1874354572" MODIFIED="1536859203162" STYLE="bubble" TEXT="StageL RI">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="WreatheSystem" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1536858139227" ID="ID_1935524726" MODIFIED="1536859203162" STYLE="bubble" TEXT="Specifications">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="WreatheSystem" SIZE="14"/>
+<node COLOR="#111111" CREATED="1536858705255" ID="ID_1373169532" MODIFIED="1536859203162" STYLE="bubble" TEXT="EITE">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="WreatheSystem" SIZE="12"/>
+<node COLOR="#111111" CREATED="1536858734795" ID="ID_1367894377" MODIFIED="1536859203162" STYLE="bubble" TEXT="...">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="WreatheSystem" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1536858732164" ID="ID_1542443348" MODIFIED="1536859203162" STYLE="bubble" TEXT="Structured data">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="WreatheSystem" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1536858715037" ID="ID_751418031" MODIFIED="1536859203162" STYLE="bubble" TEXT="Traditional text">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="WreatheSystem" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1536858700878" ID="ID_1212595932" MODIFIED="1536859203162" STYLE="bubble" TEXT="StageL">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="WreatheSystem" SIZE="12"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1536858046153" ID="ID_1667797543" MODIFIED="1536859203162" STYLE="bubble" TEXT="Data storage">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="WreatheSystem" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1536858038527" ID="ID_1176166653" MODIFIED="1536859203162" STYLE="bubble" TEXT="Development principles">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="WreatheSystem" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1536858053808" ID="ID_288664882" MODIFIED="1536859203161" STYLE="bubble" TEXT="Server apps">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="WreatheSystem" SIZE="16"/>
+<node COLOR="#990000" CREATED="1536858159183" ID="ID_1899247428" MODIFIED="1536859203161" STYLE="bubble" TEXT="Public-facing API">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="WreatheSystem" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1536858165321" ID="ID_371755692" MODIFIED="1536859203161" STYLE="bubble" TEXT="Search engine">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="WreatheSystem" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1536858170866" ID="ID_524170277" MODIFIED="1536859203161" STYLE="bubble" TEXT="Storage backend">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="WreatheSystem" SIZE="14"/>
+</node>
+</node>
+</node>
 <node COLOR="#0033ff" CREATED="1536857809483" ID="ID_1912937323" MODIFIED="1536859203162" POSITION="right" STYLE="bubble" TEXT="Library">
 <edge COLOR="#808080" STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="WreatheSystem" SIZE="18"/>
@@ -28,158 +176,10 @@
 <font NAME="WreatheSystem" SIZE="16"/>
 </node>
 </node>
-<node COLOR="#0033ff" CREATED="1536857814458" ID="ID_1475420914" MODIFIED="1536859203162" POSITION="right" STYLE="bubble" TEXT="Information technology environment">
-<edge COLOR="#808080" STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="WreatheSystem" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1536858038527" ID="ID_1176166653" MODIFIED="1536859203162" STYLE="bubble" TEXT="Development principles">
-<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
-<font NAME="WreatheSystem" SIZE="16"/>
-</node>
-<node COLOR="#00b439" CREATED="1536858043185" ID="ID_879988637" MODIFIED="1536859203162" STYLE="bubble" TEXT="Data formats">
-<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
-<font NAME="WreatheSystem" SIZE="16"/>
-<node COLOR="#990000" CREATED="1536858139227" ID="ID_1935524726" MODIFIED="1536859203162" STYLE="bubble" TEXT="Specifications">
-<edge COLOR="#808080" STYLE="bezier"/>
-<font NAME="WreatheSystem" SIZE="14"/>
-<node COLOR="#111111" CREATED="1536858700878" ID="ID_1212595932" MODIFIED="1536859203162" STYLE="bubble" TEXT="StageL">
-<edge COLOR="#808080" STYLE="bezier"/>
-<font NAME="WreatheSystem" SIZE="12"/>
-</node>
-<node COLOR="#111111" CREATED="1536858705255" ID="ID_1373169532" MODIFIED="1536859203162" STYLE="bubble" TEXT="EITE">
-<edge COLOR="#808080" STYLE="bezier"/>
-<font NAME="WreatheSystem" SIZE="12"/>
-<node COLOR="#111111" CREATED="1536858715037" ID="ID_751418031" MODIFIED="1536859203162" STYLE="bubble" TEXT="Traditional text">
-<edge COLOR="#808080" STYLE="bezier"/>
-<font NAME="WreatheSystem" SIZE="12"/>
-</node>
-<node COLOR="#111111" CREATED="1536858732164" ID="ID_1542443348" MODIFIED="1536859203162" STYLE="bubble" TEXT="Structured data">
-<edge COLOR="#808080" STYLE="bezier"/>
-<font NAME="WreatheSystem" SIZE="12"/>
-</node>
-<node COLOR="#111111" CREATED="1536858734795" ID="ID_1367894377" MODIFIED="1536859203162" STYLE="bubble" TEXT="...">
-<edge COLOR="#808080" STYLE="bezier"/>
-<font NAME="WreatheSystem" SIZE="12"/>
-</node>
-</node>
-</node>
-<node COLOR="#990000" CREATED="1536858144616" ID="ID_604776499" MODIFIED="1536859203162" STYLE="bubble" TEXT="Reference implementations">
-<edge COLOR="#808080" STYLE="bezier"/>
-<font NAME="WreatheSystem" SIZE="14"/>
-<node COLOR="#111111" CREATED="1536858653579" ID="ID_1874354572" MODIFIED="1536859203162" STYLE="bubble" TEXT="StageL RI">
-<edge COLOR="#808080" STYLE="bezier"/>
-<font NAME="WreatheSystem" SIZE="12"/>
-</node>
-<node COLOR="#111111" CREATED="1536858747930" ID="ID_1465504453" MODIFIED="1536859203162" STYLE="bubble" TEXT="EITE document format RI">
-<edge COLOR="#808080" STYLE="bezier"/>
-<font NAME="WreatheSystem" SIZE="12"/>
-<node COLOR="#111111" CREATED="1536858755982" ID="ID_199768905" MODIFIED="1536859203162" STYLE="bubble" TEXT="Dc list form">
-<edge COLOR="#808080" STYLE="bezier"/>
-<font NAME="WreatheSystem" SIZE="12"/>
-</node>
-<node COLOR="#111111" CREATED="1536858845994" ID="ID_245871844" MODIFIED="1536859203162" STYLE="bubble" TEXT="Text source form">
-<edge COLOR="#808080" STYLE="bezier"/>
-<font NAME="WreatheSystem" SIZE="12"/>
-</node>
-</node>
-</node>
-</node>
-<node COLOR="#00b439" CREATED="1536858046153" ID="ID_1667797543" MODIFIED="1536859203162" STYLE="bubble" TEXT="Data storage">
-<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
-<font NAME="WreatheSystem" SIZE="16"/>
-</node>
-<node COLOR="#00b439" CREATED="1536858051722" ID="ID_1387187635" MODIFIED="1536859203161" STYLE="bubble" TEXT="Client app">
-<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
-<font NAME="WreatheSystem" SIZE="16"/>
-<node COLOR="#990000" CREATED="1536858873075" ID="ID_1218950878" MODIFIED="1536859203161" STYLE="bubble" TEXT="Simple text rendering">
-<edge COLOR="#808080" STYLE="bezier"/>
-<font NAME="WreatheSystem" SIZE="14"/>
-</node>
-<node COLOR="#990000" CREATED="1536858966592" ID="ID_1121684754" MODIFIED="1536859203161" STYLE="bubble" TEXT="StageL in EITE">
-<edge COLOR="#808080" STYLE="bezier"/>
-<font NAME="WreatheSystem" SIZE="14"/>
-</node>
-<node COLOR="#990000" CREATED="1536858977744" ID="ID_378862452" MODIFIED="1536859203161" STYLE="bubble" TEXT="UI widgets">
-<edge COLOR="#808080" STYLE="bezier"/>
-<font NAME="WreatheSystem" SIZE="14"/>
-</node>
-<node COLOR="#990000" CREATED="1536858982884" ID="ID_1689569" MODIFIED="1536859203161" STYLE="bubble" TEXT="Simple document editor">
-<edge COLOR="#808080" STYLE="bezier"/>
-<font NAME="WreatheSystem" SIZE="14"/>
-</node>
-<node COLOR="#990000" CREATED="1536858987509" ID="ID_1080877014" MODIFIED="1536859203161" STYLE="bubble" TEXT="Complex text rendering">
-<edge COLOR="#808080" STYLE="bezier"/>
-<font NAME="WreatheSystem" SIZE="14"/>
-</node>
-<node COLOR="#990000" CREATED="1536858993958" ID="ID_91462065" MODIFIED="1536859203161" STYLE="bubble" TEXT="Networking">
-<edge COLOR="#808080" STYLE="bezier"/>
-<font NAME="WreatheSystem" SIZE="14"/>
-</node>
-<node COLOR="#990000" CREATED="1536858998764" ID="ID_1291510183" MODIFIED="1536859203161" STYLE="bubble" TEXT="Data storage and distribution">
-<edge COLOR="#808080" STYLE="bezier"/>
-<font NAME="WreatheSystem" SIZE="14"/>
-</node>
-<node COLOR="#990000" CREATED="1536859002631" ID="ID_692537468" MODIFIED="1536859203161" STYLE="bubble" TEXT="...">
-<edge COLOR="#808080" STYLE="bezier" WIDTH="2"/>
-<font NAME="WreatheSystem" SIZE="14"/>
-</node>
-</node>
-<node COLOR="#00b439" CREATED="1536858053808" ID="ID_288664882" MODIFIED="1536859203161" STYLE="bubble" TEXT="Server apps">
-<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
-<font NAME="WreatheSystem" SIZE="16"/>
-<node COLOR="#990000" CREATED="1536858159183" ID="ID_1899247428" MODIFIED="1536859203161" STYLE="bubble" TEXT="Public-facing API">
-<edge COLOR="#808080" STYLE="bezier"/>
-<font NAME="WreatheSystem" SIZE="14"/>
-</node>
-<node COLOR="#990000" CREATED="1536858165321" ID="ID_371755692" MODIFIED="1536859203161" STYLE="bubble" TEXT="Search engine">
-<edge COLOR="#808080" STYLE="bezier"/>
-<font NAME="WreatheSystem" SIZE="14"/>
-</node>
-<node COLOR="#990000" CREATED="1536858170866" ID="ID_524170277" MODIFIED="1536859203161" STYLE="bubble" TEXT="Storage backend">
-<edge COLOR="#808080" STYLE="bezier"/>
-<font NAME="WreatheSystem" SIZE="14"/>
-</node>
-</node>
-</node>
-<node COLOR="#0033ff" CREATED="1536857827313" ID="ID_1663966613" MODIFIED="1536859203161" POSITION="right" STYLE="bubble" TEXT="Community">
-<edge COLOR="#808080" STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="WreatheSystem" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1536858230493" ID="ID_776110827" MODIFIED="1536859203161" STYLE="bubble" TEXT="Community development process">
-<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
-<font NAME="WreatheSystem" SIZE="16"/>
-<node COLOR="#990000" CREATED="1536858247347" ID="ID_1022438097" MODIFIED="1536859203161" STYLE="bubble" TEXT="Human support">
-<edge COLOR="#808080" STYLE="bezier"/>
-<font NAME="WreatheSystem" SIZE="14"/>
-</node>
-<node COLOR="#990000" CREATED="1536858262370" ID="ID_1027927813" MODIFIED="1536859203161" STYLE="bubble" TEXT="Money">
-<edge COLOR="#808080" STYLE="bezier"/>
-<font NAME="WreatheSystem" SIZE="14"/>
-</node>
-<node COLOR="#990000" CREATED="1536858271888" ID="ID_1204484055" MODIFIED="1536859203161" STYLE="bubble" TEXT="Stability">
-<edge COLOR="#808080" STYLE="bezier"/>
-<font NAME="WreatheSystem" SIZE="14"/>
-</node>
-</node>
-<node COLOR="#00b439" CREATED="1536858327650" ID="ID_323325431" MODIFIED="1536859203161" STYLE="bubble" TEXT="Specification development work">
-<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
-<font NAME="WreatheSystem" SIZE="16"/>
-</node>
-<node COLOR="#00b439" CREATED="1536858338680" ID="ID_824587110" MODIFIED="1536859203161" STYLE="bubble" TEXT="Technology development work">
-<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
-<font NAME="WreatheSystem" SIZE="16"/>
-</node>
-<node COLOR="#00b439" CREATED="1536858237135" ID="ID_1543965600" MODIFIED="1536859203161" STYLE="bubble" TEXT="Land">
-<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
-<font NAME="WreatheSystem" SIZE="16"/>
-</node>
-<node COLOR="#00b439" CREATED="1536858299231" ID="ID_840067514" MODIFIED="1536859203161" STYLE="bubble" TEXT="Infrastructure">
-<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
-<font NAME="WreatheSystem" SIZE="16"/>
-</node>
-</node>
 <node COLOR="#0033ff" CREATED="1536857834639" ID="ID_1199333014" MODIFIED="1536859203161" POSITION="right" STYLE="bubble" TEXT="Specification">
 <edge COLOR="#808080" STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="WreatheSystem" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1536858365523" ID="ID_378683101" MODIFIED="1536859203161" STYLE="bubble" TEXT="Library">
+<node COLOR="#00b439" CREATED="1536858388634" ID="ID_1938410848" MODIFIED="1536859203161" STYLE="bubble" TEXT="Community">
 <edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
 <font NAME="WreatheSystem" SIZE="16"/>
 </node>
@@ -187,22 +187,22 @@
 <edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
 <font NAME="WreatheSystem" SIZE="16"/>
 </node>
-<node COLOR="#00b439" CREATED="1536858388634" ID="ID_1938410848" MODIFIED="1536859203161" STYLE="bubble" TEXT="Community">
+<node COLOR="#00b439" CREATED="1536858365523" ID="ID_378683101" MODIFIED="1536859203161" STYLE="bubble" TEXT="Library">
 <edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
 <font NAME="WreatheSystem" SIZE="16"/>
 </node>
 <node COLOR="#00b439" CREATED="1536858392409" ID="ID_794710578" MODIFIED="1536859203161" STYLE="bubble" TEXT="Specification">
 <edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
 <font NAME="WreatheSystem" SIZE="16"/>
+<node COLOR="#990000" CREATED="1536858619536" ID="ID_1675768316" MODIFIED="1536859203161" STYLE="bubble" TEXT="...">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="WreatheSystem" SIZE="14"/>
+</node>
 <node COLOR="#990000" CREATED="1536858593649" ID="ID_167363069" MODIFIED="1536859203161" STYLE="bubble" TEXT="Design principles">
 <edge COLOR="#808080" STYLE="bezier"/>
 <font NAME="WreatheSystem" SIZE="14"/>
 </node>
 <node COLOR="#990000" CREATED="1536858596793" ID="ID_1992744108" MODIFIED="1536859203161" STYLE="bubble" TEXT="Licensing">
-<edge COLOR="#808080" STYLE="bezier"/>
-<font NAME="WreatheSystem" SIZE="14"/>
-</node>
-<node COLOR="#990000" CREATED="1536858619536" ID="ID_1675768316" MODIFIED="1536859203161" STYLE="bubble" TEXT="...">
 <edge COLOR="#808080" STYLE="bezier"/>
 <font NAME="WreatheSystem" SIZE="14"/>
 </node>
